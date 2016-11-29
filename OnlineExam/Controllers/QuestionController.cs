07a32c289b5c4b7f23c01928a6785071078c9be8
vikas <vikas.ms@subhanu.com>
2016-11-29 15:09:36 +0530

    Initial Commit

---
+++ OnlineExam/Controllers/QuestionController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MySql.Data.MySqlClient;
+using OnlineExam.Models;
+using System.Data;
+
+namespace OnlineExam.Controllers
+{
+    public class QuestionController : Controller
+    {
+        // GET: Question
+        public MySqlConnection con=new MySqlConnection("Server=localhost;Database=online;Uid=root;Pwd=root;");
+        
+        public List<Question> disp()
+        {
+            var listofQuestions = new List<Question>();
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = string.Format("Select * from onlinetest");
+            con.Open();
+            MySqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                Question obj = new Question();
+                obj.QId = Convert.ToInt32(dr["QNo"]);
+                obj.Quest = dr["Ques"].ToString();
+                obj.Option1 = dr["Option1"].ToString();
+                obj.Option2 = dr["Option2"].ToString();
+                obj.Option3 = dr["Option3"].ToString();
+                obj.Option4 = dr["Option4"].ToString();
+                obj.answer = dr["Answer"].ToString();
+                listofQuestions.Add(obj);
+            }
+            return listofQuestions;   
+        }
+        public ActionResult Display()
+        {
+            return View(disp());
+        }
+
+        public void evaluate()
+        {
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = string.Format("Select ")
+        }
+
+    }
+}
---
+++ OnlineExam/Models/Question.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+
+namespace OnlineExam.Models
+{
+    public class Question
+    {
+        public int QId { get; set; }
+        public string Quest { get; set; }
+        public string Option1 { get; set; }
+        public string Option2 { get; set; }
+        public string Option3 { get; set; }
+        public string Option4 { get; set; }
+        public string answer { get; set; }
+    }
+
+    
+}
